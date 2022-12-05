949e160d782a90b67e477868f682f394c9530938
davidpelo <65370511+DavidPelo@users.noreply.github.com>
2022-12-05 17:45:32 -0500

    completed section 3, added linting

--- index.js
+++ index.js
@@ -1,92 +1,92 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const morgan = require('morgan');
-const Contact = require('./models/contact.model');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const morgan = require('morgan')
+const Contact = require('./models/contact.model')
 
-app.use(cors());
-app.use(express.static('build'));
-app.use(express.json());
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
-);
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+)
+morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.get('/info', (request, response) => {
   Contact.find({}).then(persons => {
     response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
       <p>${new Date()}</p>
-    `);
-  });
-});
+    `)
+  })
+})
 
 app.get('/api/persons', (request, response) => {
   Contact.find({}).then(contacts => {
-    response.json(contacts);
-  });
-});
+    response.json(contacts)
+  })
+})
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
     .then(person => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
     .catch(error => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number is missing',
-    });
+    })
   }
 
   Contact.find({}).then(contacts => {
     if (contacts.find(person => person.name === body.name)) {
       return response.status(400).json({
         error: 'name must be unique',
-      });
+      })
     }
 
     const contact = new Contact({
       name: body.name,
       number: body.number,
-    });
+    })
 
     contact
       .save()
       .then(savedContact => {
-        response.json(savedContact);
+        response.json(savedContact)
       })
-      .catch(error => next(error));
-  });
-});
+      .catch(error => next(error))
+  })
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Contact.findByIdAndDelete(request.params.id)
     .then(result => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch(err => next(err));
-});
+    .catch(err => next(err))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const contact = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Contact.findByIdAndUpdate(request.params.id, contact, {
     new: true,
@@ -94,26 +94,26 @@ app.put('/api/persons/:id', (request, response, next) => {
     context: 'query',
   })
     .then(updatedContact => {
-      response.json(updatedContact);
+      response.json(updatedContact)
     })
-    .catch(error => next(error));
-});
+    .catch(error => next(error))
+})
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error);
+  console.error(error)
 
-  if (error.name === 'CastError' && error.kind == 'ObjectId') {
-    return response.status(400).send({ error: 'malformatted id' });
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,65 +1,63 @@
-require('dotenv').config();
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://dpelo:${password}@cluster0.kda7csf.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://dpelo:${password}@cluster0.kda7csf.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 const contactSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Contact = mongoose.model("Contact", contactSchema);
+const Contact = mongoose.model('Contact', contactSchema)
 
-if(process.argv.length === 3) {
-  retrieveAllContacts();
+if (process.argv.length === 3) {
+  retrieveAllContacts()
 } else if (process.argv.length === 5) {
-  postNewContact();
+  postNewContact()
 } else {
   console.log('oopsy something is weird')
 }
 
 function postNewContact() {
-  const name = process.argv[3];
-  const number = process.argv[4];
+  const name = process.argv[3]
+  const number = process.argv[4]
 
   mongoose
     .connect(url)
     .then(result => {
-      console.log("connected");
-  
+      console.log('connected')
+
       const contact = new Contact({
         name: name,
         number: number,
-      });
-  
-      return contact.save();
+      })
+
+      return contact.save()
     })
     .then(() => {
-      console.log(`added ${name} number ${number} to phonebook`);
-      return mongoose.connection.close();
+      console.log(`added ${name} number ${number} to phonebook`)
+      return mongoose.connection.close()
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 }
 
 function retrieveAllContacts() {
   mongoose.connect(url).then(() => {
     Contact.find({}).then(result => {
       console.log('phonebook:')
-      result.forEach(contact=> {
+      result.forEach(contact => {
         console.log(contact.name, contact.number)
       })
       mongoose.connection.close()
     })
   })
 }
-
-
